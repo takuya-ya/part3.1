2e90f362abaf67d89d4d526d35b07392515e170c
takuya <cdwbh564@yahoo.co.jp>
2024-10-28 22:04:51 +0900

    弁当とドリンクセットによる割引額算出の処理を追加

--- src/paymentAmount_super.php
+++ src/paymentAmount_super.php
@@ -41,29 +41,31 @@
  *  税率をかけて税込み金額を計算
  */
 
-const PRICE =  [
-  1 => 100,
-  2 => 150,
-  3 => 200,
-  4 => 350,
-  5 => 180,
-  6 => 220,
-  7 => 440,
-  8 => 380,
-  9 => 80,
-  10 => 100
+//  セット割を計算する為、typeキーを追加
+const PRODUCT_INFO =  [
+  1 => ['price' => 100, 'type' => 'material'],
+  2 => ['price' => 150, 'type' => 'material'],
+  3 => ['price' => 200, 'type' => 'material'],
+  4 => ['price' => 350, 'type' => 'material'],
+  5 => ['price' => 180, 'type' => 'drink'],
+  6 => ['price' => 220, 'type' => 'material'],
+  7 => ['price' => 440, 'type' => 'bento'],
+  8 => ['price' => 380, 'type' => 'bento'],
+  9 => ['price' => 80, 'type' => 'drink'],
+  10 => ['price' => 100, 'type' => 'drink'],
 ];
 const ONION_NUM_THREE = 3;
 const ONION_NUM_FIVE = 5;
 const ONION_DISCOUNT_THREE = 50;
 const ONION_DISCOUNT_FIVE = 100;
+const LUNCH_DISCOUNT = 20;
 
 function calcTotalPrice (array $nums): int
 {
     $prices = [];
     foreach ($nums as $num)
     {
-        $prices[] = PRICE[$num];
+        $prices[] = PRODUCT_INFO[$num]['price'];
     }
     var_dump(array_sum($prices));
     return 1;
@@ -71,32 +73,42 @@ function calcTotalPrice (array $nums): int
 
 function onion (array $countProductNums): int
 {
-
   //玉ねぎの個数に応じて、値引き額を算出
-  $TotalOnionDiscount = 0;
-  if ($countProductNums[1] >= ONION_NUM_FIVE) {
-    $TotalOnionDiscount = ONION_DISCOUNT_FIVE;
-  } elseif ($countProductNums[1] >= ONION_NUM_THREE) {
+    $TotalOnionDiscount = 0;
+    if ($countProductNums[1] >= ONION_NUM_FIVE) {
+        $TotalOnionDiscount = ONION_DISCOUNT_FIVE;
+    } elseif ($countProductNums[1] >= ONION_NUM_THREE) {
         $TotalOnionDiscount = ONION_DISCOUNT_THREE;
-      }
-      var_dump($TotalOnionDiscount);
-      return $TotalOnionDiscount;
+    }
+    var_dump($TotalOnionDiscount);
+    return $TotalOnionDiscount;
 }
 
+// セットの割引額を算出
 function lunchDiscount(array $countProductNums): int
 {
-    return 0;
+    $bento = 0;
+    $drink = 0;
+    // 弁当とドリンクカテゴリに該当する商品をカウント
+    foreach (PRODUCT_INFO as $productInfo) {
+        if ($productInfo['type'] === 'bento') {
+            $bento++;
+        } elseif ($productInfo['type'] === 'drink') {
+            $drink++;
+        }
+    }
+    // セット数をカウントして割引単価と乗算
+    return (LUNCH_DISCOUNT * min($bento, $drink));
 }
 
 function calcDiscount (array $nums): int
 {
     $discountPrice = 0;
-    // 購入した商品数を番号ごとにカウント
+    // 購入した商品数を番号毎にカウント
     $countProductNums = array_count_values($nums);
-      // 玉ねぎ複数購入の割引
-    // 弁当とドリンクのセット割引
-    // タイムセールによる割引
+    // 玉ねぎ複数購入の割引 + 弁当とドリンクのセット割引 + タイムセールによる割引
     lunchDiscount($countProductNums);
+
     // return $discountPrice= onion() + lunchDrinkSet() + timeSale();
     return 1;
 }
