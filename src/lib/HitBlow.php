537e98ea10c5cd569684cbca9bd4f774e709e396
takuya <cdwbh564@yahoo.co.jp>
2024-10-30 18:25:20 +0900

    重複していた処理を、ループの前処理として統合

--- src/lib/HitBlow.php
+++ src/lib/HitBlow.php
@@ -1,34 +1,34 @@
 <?php
 
-function hitCondition($answer, int $place, int $num)
+function hitCondition(array $arrayAnswer, int $place, int $num)
 {
-    return ((int)str_split($answer)[$place]) === $num;
+    return (int)$arrayAnswer[$place] === $num;
 }
 
-function blowCondition($answer, int $place, int $num)
+function blowCondition(array $arrayAnswer, int $place, int $num)
 {
-    return in_array($num, str_split($answer));
+    return in_array($num, $arrayAnswer);
 }
 
 function judge(int $question, int $answer): array
 {
-    $place = 0;
-    $num = 0;
     $hit = 0;
     $blow = 0;
+    // 修正：重複していたので、前処理として統合
+    $arrayAnswer = str_split($answer);
     //questionを配列にして、桁番号と数字を順次出力
     foreach(str_split($question) as $place => $num) {
         // HitかBlowの条件と合致した場合、インクリメント
-        if(hitCondition($answer, $place, $num)){
+        if(hitCondition($arrayAnswer, $place, $num)){
             $hit++;
-        } elseif (blowCondition($answer, $place, $num)) {
+        } elseif (blowCondition($arrayAnswer, $place, $num)) {
             $blow++;
         }
     }
     return [$hit, $blow];
 }
 
-judge(5684, 5684);
+// judge(5684, 5684);
 // リファクタ：foreachと条件式の変数を修正、複雑値を下げる
         // foreach(str_split(($answer)) as $answerPlace => $answerNum) {
             // Hitの条件を変数化
