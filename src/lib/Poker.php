5d8974a2c7c8ebaf1b51b2a779558c6801f68fa2
takuya <cdwbh564@yahoo.co.jp>
2024-11-01 01:46:35 +0900

    勝者を判定する処理を追加

--- src/lib/Poker.php
+++ src/lib/Poker.php
@@ -1,5 +1,7 @@
 <?php
 
+use function PHPUnit\Framework\equalTo;
+
 const CARD_RANK = [
     2 => 1,
     3 => 2,
@@ -19,34 +21,32 @@ const CARD_RANK = [
 const HIGH_CARD = 1;
 const PAIR = 2;
 const STRAIGHT = 3;
+const DRAW= 0;
+const PLAYER1 = 1;
+const PLAYER2 = 2;
+
 
 function showDown (string $card1A, string $card1B, string $card2A, string $card2B): array
 {
+    // カードを配列として取得
     $hands = [$card1A, $card1B, $card2A, $card2B];
 
-    // カードをランクに変換
+    // カードランクを取得
     $handRanks = getRank($hands);
-    // カードの役を判定
+    // カードの役を取得
     $handRoles = getRole($handRanks);
-    // 役をランクに変換
+    // 役のランクを取得
     $roleRanks = getRoleRank($handRoles);
+    // 勝者を判定
+    $winner = judgeWinner($roleRanks, $handRanks, $handRoles);
 
     /**
      * プレイやー情報[カードランク、カードランク、役ランク]
-     * 勝敗を判定
-     *      役の強さを判定
-     *          役をランク化
-     *          比較
-     *      引き分けの場合、数字の強さを判定
-     *          数字を引数から受ける
-     *          max関数で呼ぶ
-     *
      * 出力
      *   [役A、役B、勝者番号]
      */
-return ['', '', 2];
+return [$handRoles[0], $handRoles[1], $winner];
 }
-
 
 function getRank(array $hands): array
 {
@@ -70,12 +70,12 @@ function getRole(array $handRanks): array
         if ($ranks[0] === $ranks[1])
         {
             $handRoles[] = 'pair';
-        } elseif (($ranks[0] - $ranks[1]) === 1)
+        } elseif (abs($ranks[0] - $ranks[1]) === 1 || abs($ranks[0] - $ranks[1]) === 12)
         {
             $handRoles[] = 'straight';
         } else
         {
-            $handRoles[] = 'highCard';
+            $handRoles[] = 'high card';
         }
     }
     return $handRoles;
@@ -85,7 +85,7 @@ function getRoleRank(array $handRoles)
 {
     foreach($handRoles as $handRole) {
         switch($handRole) {
-            case 'highCard':
+            case 'high card':
                 $roleRanks[] = HIGH_CARD;
                 break;
             case 'pair':
@@ -99,4 +99,88 @@ function getRoleRank(array $handRoles)
     return $roleRanks;
 }
 
-showDown('CK', 'DJ', 'C10', 'H10');
+function judgeWinner(array $roleRanks, array $handRanks, array $handRoles) {
+    if ($roleRanks[0] > $roleRanks[1]) {
+        $winner = PLAYER1;
+    } elseif ($roleRanks[0] < $roleRanks[1]) {
+        $winner = PLAYER2;
+    } else {
+        // 役が同じ場合　カードのランクで比較
+        $winner = compareCardNum($handRanks, $handRoles);
+    }
+    return $winner;
+}
+
+function compareCardNum(array $handRanks, array $handRoles) {
+
+    $maxRank = MaxCardNum($handRanks);
+    $minRank = MinCardNum($handRanks);
+    // 手札で強いランク取得、弱いランク、
+    // 引き分けた役毎に行う
+    if($handRoles[0] === 'high card') {
+        if($maxRank[0] > $maxRank[1]) {
+            $winner = PLAYER1;
+        } elseif ($minRank[0] < $minRank[1]) {
+            $winner = PLAYER2;
+        } else {
+            $winner = compareCardMinNum($minRank);
+        }
+    // 強いランク同士を比較
+    } elseif ($handRoles[0] === 'pair') {
+        if($maxRank[0] > $maxRank[1]) {
+            $winner = PLAYER1;
+        } elseif ($maxRank[0] < $maxRank[1]) {
+            $winner = PLAYER2;
+        } else {
+            $winner = DRAW;
+        }
+    } elseif ($handRoles[0] === 'straight') {
+        // A-2の組み合わせの場合、2が最強
+        $value = [CARD_RANK['A'], CARD_RANK[2]];
+        $indent = 0;
+        foreach($handRanks as $handRank) {
+            if(empty(array_diff($value, $handRank))) {
+                $maxRank[$indent] = CARD_RANK[2];
+            }
+            // if文の中に入れない事。中に入れると、プレイヤー2がA-2でも、その時点でindentは0だからプレイヤー1の手札が最弱になってしまう。
+            $indent++;
+        }
+
+        if($maxRank[0] > $maxRank[1]) {
+            $winner = PLAYER1;
+        } elseif ($maxRank[0] < $maxRank[1]) {
+            $winner = PLAYER2;
+        } else {
+            $winner = DRAW;
+        }
+    }
+    return $winner;
+}
+
+function MaxCardNum($handRanks)
+{
+    foreach($handRanks as $handRank) {
+        $maxRank[] = max($handRank);
+    }
+    return $maxRank;
+}
+
+function MinCardNum($handRanks)
+{
+    foreach($handRanks as $handRank) {
+        $minRank[] = min($handRank);
+    }
+    return $minRank;
+}
+
+function compareCardMinNum($minRank) {
+    if($minRank[0] > $minRank[1]) {
+        $winner = PLAYER1;
+    } elseif ($minRank[0] < $minRank[1]) {
+        $winner = PLAYER2;
+    } else {
+        $winner = DRAW;
+    }
+    return $winner;
+}
+showDown('SA', 'DK', 'C2', 'CA');
