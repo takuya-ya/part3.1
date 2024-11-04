b9d1d224df3893f3bf6b3fc0573eeb609bb29e2e
takuya <cdwbh564@yahoo.co.jp>
2024-11-04 15:25:40 +0900

    引数のカードから、数字を抽出して配列化する処理を追加

--- src/lib/oop_poker/PokerGame.php
+++ src/lib/oop_poker/PokerGame.php
@@ -8,6 +8,13 @@ class PokerGame
 
     public function start(): array
     {
+        // カードの数字を抽出して、配列化
+        // array [[H10, H3], [D7, D9]]
+        $cardNumber = [];
+        foreach ([$this->card1, $this->card2] as $playerCards) {
+          $cardNumber[] = array_map(fn($playerCard) => substr($playerCard, 1), $playerCards);
+        }
+        
         return $answer = [[13, 13], [9, 9]];
     }
 }
