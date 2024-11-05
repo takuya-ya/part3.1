d37a0a61a225e417e0cedd6ec2a851badcaa8a3c
takuya <cdwbh564@yahoo.co.jp>
2024-11-06 00:33:51 +0900

    子クラスがコンストラクトにアクセス不可の為、アクセス権限を変更

--- src/lib/vending_machine/Item.php
+++ src/lib/vending_machine/Item.php
@@ -5,7 +5,7 @@ abstract class Item
     abstract public function getPrice();
     abstract public function getCupNumber();
 
-    public function __construct(private string $name)
+    public function __construct(public string $name)
     {
     }
 
