40fe167f81d4329518490b7c8c7f9da9337b5e7b
takuya <cdwbh564@yahoo.co.jp>
2024-11-12 16:31:38 +0900

    手札3枚の場合のテストファイル作成:ファイル内のコードは仮コード

---
+++ src/tests/oop_poker/ThreePokerJudgeRuleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OopPoker\Tests;
+
+use PHPUnit\Framework\TestCase;
+use OopPoker\ThreePokerJudgeRule;
+
+require_once(__DIR__ . '/../../lib/oop_poker/ThreePokerJudgeRule.php');
+
+class ThreePokerJudgeRuleTest extends TestCase
+{
+    public function testGetWinner()
+    {
+        $rule = new ThreePokerJudgeRule();
+        $this->assertSame(2, $rule->getWinner([['name' => 'pair', 'hand rank' => 1, 'primary' => 9, 'secondly' => 9], ['name' => 'pair', 'hand rank' => 2, 'primary' => 8, 'secondly' => 9]]));
+    }
+}
