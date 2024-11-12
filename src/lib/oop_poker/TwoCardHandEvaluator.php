5d3a5ede731a034c56a33951ece67321b0e616e4
takuya <cdwbh564@yahoo.co.jp>
2024-11-12 19:12:58 +0900

    手札2枚時の、手札評価の処理を追加

---
+++ src/lib/oop_poker/TwoCardHandEvaluator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OopPoker;
+
+class TwoCardHandEvaluator implements PokerHandEvaluator
+{
+    private const HAND_RANK = [
+        'high card' => 1,
+        'pair' => 2,
+        'straight' => 3
+    ];
+
+    public function __construct(private PokerRule $rule)
+    {
+    }
+
+    // 引数：カードインスタンスの配列
+    public function getHand(array $pokerCards): array
+    {
+        // ランク[10,9]
+        $ranks = array_map(fn($cardRank) => $cardRank->getRank(), $pokerCards);
+        // ルールインスタンスにgetHandメソッドを実行させる
+        // 役名を取得
+        $hand = $this->rule->getHand($ranks);
+        max($ranks);
+
+        // 手札がA,2 = rank:12, 1の場合 ルールに基づき、primaryに1,secondlyに12を代入
+        if($this->rule->maxMin($ranks)) {
+            $ranks[0] = min(PokerCard::CARD_RANK);
+            $ranks[1] = max(PokerCard::CARD_RANK);
+        }
+        return ['name' => $hand, 'hand rank' => self::HAND_RANK[$hand], 'primary' => $ranks[0], 'secondly' => $ranks[1]];
+    }
+}
+// $handEvaluator = new (new TwoPokerCardRule);
+// $handEvaluator->getHand([new PokerCard('H10'), new PokerCard('H10')]);
--- src/tests/oop_poker/ThreePokerJudgeRuleTest.php
+++ src/tests/oop_poker/ThreePokerJudgeRuleTest.php
@@ -12,6 +12,5 @@ class ThreePokerJudgeRuleTest extends TestCase
     public function testGetWinner()
     {
         $rule = new ThreePokerJudgeRule();
-        $this->assertSame(2, $rule->getWinner([['name' => 'pair', 'hand rank' => 1, 'primary' => 9, 'secondly' => 9], ['name' => 'pair', 'hand rank' => 2, 'primary' => 8, 'secondly' => 9]]));
     }
 }
