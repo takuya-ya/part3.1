978fc4896e6b78c960142de74a341b10f4be263e
takuya <cdwbh564@yahoo.co.jp>
2024-11-23 16:59:56 +0900

    VendingMachineV2Testにコイン投入処理を追加

--- src/tests/vending_machine_v2/VendingMachineV2Test.php
+++ src/tests/vending_machine_v2/VendingMachineV2Test.php
@@ -3,14 +3,17 @@
 namespace VendingMachineV2Tests;
 
 use PHPUnit\Framework\TestCase;
+use VendingMachineV2\CoinManager;
 use VendingMachineV2\VendingMachineV2;
-
+use VendingMachineV2\Cola;
 
 class VendingMachineV2Test extends TestCase{
 
     public function testPressButton()
     {
         $vendingMachine = new VendingMachineV2;
-        $this->assertSame('cola', $vendingMachine->pressButton('cola'));
+        $item = new Cola;
+        $vendingMachine->depositCoin(100);
+        $this->assertSame('cola', $vendingMachine->pressButton($item));
     }
 }
