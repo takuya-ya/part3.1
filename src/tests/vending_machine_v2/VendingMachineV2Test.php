cf707eb18207996d9cbdb464b69bd72242a204c9
takuya <cdwbh564@yahoo.co.jp>
2024-11-23 17:13:26 +0900

    testPressButtonに投入金額不足時のテストケースを追加

--- src/tests/vending_machine_v2/VendingMachineV2Test.php
+++ src/tests/vending_machine_v2/VendingMachineV2Test.php
@@ -3,7 +3,6 @@
 namespace VendingMachineV2Tests;
 
 use PHPUnit\Framework\TestCase;
-use VendingMachineV2\CoinManager;
 use VendingMachineV2\VendingMachineV2;
 use VendingMachineV2\Cola;
 
@@ -13,7 +12,15 @@ class VendingMachineV2Test extends TestCase{
     {
         $vendingMachine = new VendingMachineV2;
         $item = new Cola;
+
+        // 投入金額が100円未満の場合、コーラは購入できない
+        $vendingMachine->depositCoin(0);
+        $this->assertSame('お金が足りません', $vendingMachine->pressButton($item));
+
+        // 投入金額が100円の場合、コーラが購入できる
         $vendingMachine->depositCoin(100);
         $this->assertSame('cola', $vendingMachine->pressButton($item));
+
+
     }
 }
