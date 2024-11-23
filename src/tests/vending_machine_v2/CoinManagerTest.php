8cfeac8f1fc8fe7734a1cc78afca0ad87695196e
takuya <cdwbh564@yahoo.co.jp>
2024-11-23 17:02:53 +0900

    CoinManagerTestにuseCoinメソッドのテストを追加

--- src/tests/vending_machine_v2/CoinManagerTest.php
+++ src/tests/vending_machine_v2/CoinManagerTest.php
@@ -4,6 +4,7 @@ namespace VendingMachineV2Tests;
 
 use PHPUnit\Framework\TestCase;
 use VendingMachineV2\CoinManager;
+use VendingMachineV2\Cola;
 
 class CoinManagerTest extends TestCase
 {
@@ -14,5 +15,16 @@ class CoinManagerTest extends TestCase
         $this->assertSame(0, $coinManager->depositCoin(50));
         // 投入金額が100円の場合、投入金額が保持される
         $this->assertSame(100, $coinManager->depositCoin(100));
+    }
+
+    public function testUseCoin()
+    {
+        $coinManager = new CoinManager();
+        // 投入金額が100円未満の場合、コーラは購入できない
+        $coinManager->depositCoin(50);
+        $this->assertSame(false, $coinManager->useCoin(new Cola));
+        // 投入金額が100円の場合、コーラが購入できる
+        $coinManager->depositCoin(100);
+        $this->assertSame(true, $coinManager->useCoin(new Cola));
     }
 }
