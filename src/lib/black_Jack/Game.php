af84a392669e316db34a50d3a92768c4cb18ca77
takuya <cdwbh564@yahoo.co.jp>
2024-12-23 22:01:46 +0900

    feat:start関数を作成
    初期テストを通すための簡易的なstart関数を追加

--- src/lib/black_Jack/Game.php
+++ src/lib/black_Jack/Game.php
@@ -4,7 +4,12 @@ namespace BlackJack;
 
 class Game
 {
-      public function __construct(public string $playerName)
+    public function __construct(public string $playerName)
     {
+    }
+
+    public function start()
+    {
+        echo $this->playerName;
     }
 }
