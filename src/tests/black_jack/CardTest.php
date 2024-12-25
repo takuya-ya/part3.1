1b87dfcae38344733feda7f70ecc8d0f4a17e227
takuya <cdwbh564@yahoo.co.jp>
2024-12-25 14:06:59 +0900

    feat;constructを実装し、カードを追加する処理を実装
    テスト通す為、簡易的な処理のみ実装

--- src/tests/black_jack/CardTest.php
+++ src/tests/black_jack/CardTest.php
@@ -5,6 +5,8 @@ namespace BlackJack\Tests;
 use PHPUnit\Framework\TestCase;
 use BlackJack\Card;
 
+require_once(__DIR__ . '/../../lib/black_jack/Card.php');
+
 class CardTest extends TestCase
 {
     public function testCardConstruct()
