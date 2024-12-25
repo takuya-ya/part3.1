4406a29a3f52dd07d0cea3b7a85f1aec71d119bd
takuya <cdwbh564@yahoo.co.jp>
2024-12-25 17:57:43 +0900

    featDeckTest.phpを作成し、初回テストコードを実装
    - テストコードを通すための簡易ロジックを実装

--- src/lib/black_jack/deck.php
+++ src/lib/black_jack/deck.php
@@ -6,7 +6,6 @@ class Deck
 {
     public function drawCard()
     {
-
+        
     }
 }
-
---
+++ src/tests/black_jack/DeckTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BlackJack\Tests;
+
+use PHPUnit\Framework\TestCase;
+use BlackJack\Deck;
+use BlackJack\Card;
+
+require_once(__DIR__ . '/../../lib/black_jack/Deck.php');
+
+class DeckTest extends TestCase
+{
+    public function testDrawCard()
+    {
+        $deck = new Deck;
+        $card = $deck->drawCard();
+        $this->assertSame('S2', $card);
+    }
+}
