76578617c21fa748f6b743cea933af629e01401c
takuya <cdwbh564@yahoo.co.jp>
2024-12-25 16:18:51 +0900

    feat;カードデッキを作成する処理を追加
    ゲームの為、スートとカード番号を組み合わせ、トランプのカードを全種作成した

--- src/lib/black_jack/Card.php
+++ src/lib/black_jack/Card.php
@@ -7,6 +7,13 @@ class Card
   public $cards = [];
   public function __construct()
   {
-      $this->cards[] = 'S1';
+      $suits = ['S', 'H', 'D', 'K'];
+      $cardNumber = array_merge(range(2,10),['J', 'Q', 'K', 'A']);
+
+      foreach ($suits as $suit) {
+        foreach ( $cardNumber as $cardNum) {
+            $this->cards[] = "$suit"."$cardNum";
+        }
+      }
   }
 }
