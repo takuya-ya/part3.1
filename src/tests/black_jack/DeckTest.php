64dc13b6c611a9e1232927ef8664ba4ec2125a8e
takuya <cdwbh564@yahoo.co.jp>
2024-12-27 19:42:18 +0900

    feat:drawCardのテストコードを本実装
    - テスト内容:
     1. 返り値の型が配列である事を確認
     2.プレイヤー人数分のカードの有無を確認
     3.各プレイヤーが2枚の手札を持つ事を確認
     4.各プレイヤーに重複したカードが無い事を確認

--- src/tests/black_jack/DeckTest.php
+++ src/tests/black_jack/DeckTest.php
@@ -12,7 +12,22 @@ class DeckTest extends TestCase
     public function testDrawCard()
     {
         $deck = new Deck;
-        $card = $deck->drawCard();
-        $this->assertSame('S2', $card);
+        $playerCards = $deck->drawCard();
+
+        // 型の確認
+        $this->assertSame('array', gettype($playerCards));
+        // プレイヤー人数1名とディーラーの手札の有無を確認
+        // TODO:拡張対応、人数が増える場合の対応
+        $this->assertSame(2, count($playerCards));
+        // 1人2枚ずつ、カード有無を確認
+        $this->assertSame(2, count($playerCards[0]));
+        $this->assertSame(2, count($playerCards[1]));
+        // 重複チェック:全プレイヤーの手札カードを配列に格納して重複チェック
+        // TODO:拡張対応、人数が増える場合の対応
+        foreach ($playerCards as $playerCard){
+            foreach ($playerCard as $card)
+            $checkCards[] = $card;
+        }
+        $this->assertSame(4, count(array_unique($checkCards)));
     }
 }
