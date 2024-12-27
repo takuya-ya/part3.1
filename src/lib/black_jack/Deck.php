a414d44e16b0847d389157d94001954268cdf1e4
takuya <cdwbh564@yahoo.co.jp>
2024-12-27 12:34:17 +0900

    feat:drawCard関数を実装
    - 52枚のカードでデッキを初期化
    - デッキをシャッフル
    - 2枚単位でカードを分割
    - テストは未実装

--- src/lib/black_jack/Deck.php
+++ src/lib/black_jack/Deck.php
@@ -2,11 +2,24 @@
 
 namespace BlackJack;
 
+use BlackJack\Card;
+
+require_once(__DIR__.'/Card.php');
+
 class Deck
 {
     public function drawCard()
     {
+        $playerCards = [];
+
+        // 52枚のカードを持つ配列のプロパティを初期設定
         $card = new Card;
-        return $card->cards[0];
+        // カードをシャッフル処理
+        shuffle($card->cards);
+        // カードを手札用に2枚単位で分割
+        $playerCards = array_chunk(array_slice($card->cards, 0, 4), 2);
     }
 }
+
+// $deck = new Deck;
+// $deck->drawCard();
