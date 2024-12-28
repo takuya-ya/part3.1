afe895a7af2679881d68f11079f0615ca6c8cb46
takuya <cdwbh564@yahoo.co.jp>
2024-12-29 01:02:13 +0900

    ref:プレイヤー名の取得を、関数の引数で取得する処理に変更
    - 直書きしていたプレイヤー名を削除
    - 引数にプレイヤー名を渡し、汎用性を向上
    - 不要なコメントを削除

--- src/lib/black_jack/Dealer.php
+++ src/lib/black_jack/Dealer.php
@@ -8,12 +8,10 @@ require_once(__DIR__.'/Deck.php');
 class Dealer
 {
     public array $dealerCard = [];
-    public function dealingCard(): array
+    public function dealingCard(string $playerName): array
     {
-        // TODO:$playerName仮実装。本実装では、dealingCardの引数に設定すること。
-        $playerName = ['takuya', 'dealer'];
-
-        $playersCard = [];
+        $playerNames = ["$playerName", 'dealer'];
+        $playerCards = [];
         // TODO:プレイヤー数増加への対応追加
         // インデント番号が最後の手札はdearlerの手札配列とする
         $indent = 0;
@@ -22,14 +20,10 @@ class Dealer
         $collectionTwoCards = $deck->drawCard();
         // $playersCard配列のキーにプレイヤー名を追加し、プレイヤーと手札を紐づける
         foreach ($collectionTwoCards as $TwoCards) {
-            // TODO:dealerへのカードを渡す方法が確定次第、if文削除
-            // if (count($playersCard) - count($playerName) === 0 ) {
-            //     $dealerCard = $TwoCards;
-            //     break;
-            // }
-            $playersCard[$playerName[$indent]] = $TwoCards;
+           
+            $playerCards[$playerNames[$indent]] = $TwoCards;
             ++$indent;
         }
-        return $playersCard;
+        return $playerCards;
     }
 }
