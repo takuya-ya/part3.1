6e3ceeeae03f275ed66ae68baeff8f5b39e75929
takuya <cdwbh564@yahoo.co.jp>
2024-12-29 01:19:19 +0900

    feat:`Game.php`の変更に合わせて、プレイヤーのカードを確認するテストに変更。
    - 返り値の型が配列である事を確認
    - プレイヤー人数分の手札の確認
    - 各プレイヤーが2枚の手札を持つ事を確認

--- src/tests/black_jack/GameTest.php
+++ src/tests/black_jack/GameTest.php
@@ -12,7 +12,14 @@ class GameTest extends TestCase
     public function testStart()
     {
         $game = new Game('takuya');
-        $result = $game->start();
-        $this->assertSame('takuya', $result);
+        $playerCards = $game->start();
+        // 型の確認
+        $this->assertSame('array', gettype($playerCards));
+        // 人数分の手札の確認
+        $this->assertSame(2, count($playerCards));
+        // 各プレイヤーの手札枚数の確認
+        foreach($playerCards as $playerCard) {
+        $this->assertSame(2, count($playerCard));
+        }
     }
 }
