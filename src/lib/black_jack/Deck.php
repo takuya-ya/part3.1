853337ae2709b666d564f0b0a0207305dbc2204f
takuya <cdwbh564@yahoo.co.jp>
2024-12-28 14:14:54 +0900

    ref:コメントを修正
    - コメントで概要を把握出来るようにするため

--- src/lib/black_jack/Deck.php
+++ src/lib/black_jack/Deck.php
@@ -8,6 +8,7 @@ require_once(__DIR__.'/Card.php');
 
 class Deck
 {
+    // TODO:人数増加に対応させる
     public function drawCard()
     {
         $playerCards = [];
@@ -16,7 +17,8 @@ class Deck
         $card = new Card;
         // カードをシャッフル処理
         shuffle($card->cards);
-        // カードを手札用に2枚単位で分割
+        // ディーラー含む２名分のカード４枚をスライス
+        // カードを手札用に2枚単位で分割してプレイヤー２名分のカードを準備
         $playerCards = array_chunk(array_slice($card->cards, 0, 4), 2);
         return $playerCards;
     }
