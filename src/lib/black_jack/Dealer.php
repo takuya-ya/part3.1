33261a2c00cbd008a0e3eabf334cb6589de0306c
takuya <cdwbh564@yahoo.co.jp>
2025-01-02 22:40:10 +0900

    ref:`dealingCarr`メソッドの責任分割の為、`drawCardsForPlayer`メソッドの実装とテストコードを追加

--- src/lib/black_jack/Dealer.php
+++ src/lib/black_jack/Dealer.php
@@ -8,21 +8,27 @@ require_once(__DIR__.'/Deck.php');
 class Dealer
 {
     public array $dealerCard = [];
-    public function dealingCard(string $playerName): array
+
+    // $playerNamesにはdealerも格納済み
+    public function drawCardsForPlayer(array $playerNames, Deck $deck): array
     {
-        $playerNames = ["$playerName", 'dealer'];
         $playerCards = [];
         // TODO:プレイヤー数増加への対応追加
         // インデント番号が最後の手札はdearlerの手札配列とする
         $indent = 0;
         // カードを山札から引く処理
-        $deck = new Deck;
         $collectionTwoCards = $deck->drawCard();
         // $playersCard配列のキーにプレイヤー名を追加し、プレイヤーと手札を紐づける
         foreach ($collectionTwoCards as $TwoCards) {
             $playerCards[$playerNames[$indent]] = $TwoCards;
             ++$indent;
         }
+        return $playerCards;
+    }
+    
+    public function dealingCard(array $playerNames, Deck $deck): array
+    {
+        $playerCards = $this->drawCardsForPlayer($playerNames, $deck);
         return $playerCards;
     }
 }
--- src/tests/black_jack/DealerTest.php
+++ src/tests/black_jack/DealerTest.php
@@ -4,17 +4,40 @@ namespace BlackJack\Tests;
 
 use PHPUnit\Framework\TestCase;
 use BlackJack\Dealer;
+use BlackJack\Deck;
 
 require_once(__DIR__ . '/../../lib/black_jack/Dealer.php');
+require_once(__DIR__ . '/../../lib/black_jack/Deck.php');
 
 class DealerTest extends TestCase
 {
+    public function testDrawCardForPlayer()
+    {
+        $mockDeck = $this->createMock(Deck::class);
+        $mockDeck->method('drawCard')->willReturn([
+            ["H1", "D1"],
+            ["H2", "D2"]
+        ]);
+        // print_r($mockDeck->drawCard());
+
+        $dealer = new Dealer;
+        $playerCard = $dealer->drawCardsForPlayer(['takuya', 'dealer'], $mockDeck);
+        $this->assertSame(['H1', 'D1'], $playerCard['takuya']);
+    }
+
     public function testDealingCard()
     {
+        $mockDeck = $this->createMock(Deck::class);
+        $mockDeck->method('drawCard')->willReturn([
+            ["H1", "D1"],
+            ["H2", "D2"]
+        ]);
+
+        // print_r($mockDeck->drawCard());
         $dealer = new Dealer;
         // プレイヤー達のカードの手札が格納された配列を返す
         // $playersCard[名前]=各プレイヤーのカード
-        $playersCard = $dealer->dealingCard('takuya');
+        $playersCard = $dealer->dealingCard(['takuya','dealer'], $mockDeck);
         // 型の確認
         $this->assertSame('array', gettype($playersCard));
         // 人数分の手札の確認
@@ -22,6 +45,9 @@ class DealerTest extends TestCase
         // 各プレイヤーの手札枚数の確認
         foreach($playersCard as $playerCard) {
         $this->assertSame(2, count($playerCard));
+        // プレイヤーとカードが正常に紐づけられているかの確認
+        $this->assertSame(["H1", "D1"], $playersCard['takuya']);
+        $this->assertSame(["H2", "D2"], $playersCard['dealer']);
         }
     }
 }
