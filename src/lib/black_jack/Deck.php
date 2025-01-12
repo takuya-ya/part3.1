8ab9083979f445050255360615299525a72b5c64
takuya <cdwbh564@yahoo.co.jp>
2025-01-12 15:54:40 +0900

    feat:初回手札を取得し、プレイヤーと紐づける処理を追加

--- src/lib/black_jack/Deck.php
+++ src/lib/black_jack/Deck.php
@@ -9,6 +9,8 @@ require_once(__DIR__.'/Card.php');
 class Deck
 {
     public array $cardDeck = [];
+    public array $drawnCards = [];
+
     public function __construct(public Card $card)
     {
     }
@@ -28,5 +30,15 @@ class Deck
         // 山札からカードを取得し、取得したカードは山札から削除
         $drawnCard = array_splice($this->cardDeck, 0, $drawCardNum);
         return $drawnCard;
+    }
+
+    public function getStartHands(array $playerNames) : array
+    {
+        $playerHands = [];
+        foreach($playerNames as $playerName) {
+            $this->drawCard(2);
+            $playerHands[$playerName] = $this->drawnCards;
+        }
+        return $playerHands;
     }
 }
--- src/tests/black_jack/DeckTest.php
+++ src/tests/black_jack/DeckTest.php
@@ -33,4 +33,14 @@ class DeckTest extends TestCase
         $this->assertSame(false, in_array($drawnCard[1], $deck->cardDeck));
     }
 
+    public function testGetStartHands()
+    {
+        $deck = new Deck(new Card);
+        $deck->makeDeck();
+        $playerHands = $deck->getStartHands(['takuya', 'takashi']);
+
+        // 手札枚数の確認
+        $this->assertSame(2, count($playerHands['takuya']));
+        $this->assertSame(2, count($playerHands['takashi']));
+    }
 }
