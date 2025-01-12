bfb56fb54b6d850a1a2506042e729dcc23cf982d
takuya <cdwbh564@yahoo.co.jp>
2025-01-12 20:04:36 +0900

    ref:山札作成をDeckクラスのconstructに設定
    - 山札作成を担っていたmakeDeck関数を削除し、コードの簡潔性を向上

--- src/lib/black_jack/Deck.php
+++ src/lib/black_jack/Deck.php
@@ -9,27 +9,19 @@ require_once(__DIR__.'/Card.php');
 class Deck
 {
     public array $cardDeck = [];
-    public array $drawnCards = [];
+    public array $drawnCard = [];
 
     public function __construct(public Card $card)
     {
-    }
-
-    public function makeDeck()
-    //  : void
-    {
-        // 52枚のカードを持つ配列のプロパティを初期設定
-        // カードをシャッフル処理
         shuffle($this->card->cards);
         $this->cardDeck = $this->card->cards;
-        return $this->cardDeck;
     }
 
     public function drawCard(int $drawCardNum) : array
     {
         // 山札からカードを取得し、取得したカードは山札から削除
-        $drawnCard = array_splice($this->cardDeck, 0, $drawCardNum);
-        return $drawnCard;
+        $this->drawnCard = array_splice($this->cardDeck, 0, $drawCardNum);
+        return $this->drawnCard;
     }
 
     public function startHands(array $playerNames) : array
@@ -37,7 +29,7 @@ class Deck
         $playerHands = [];
         foreach($playerNames as $playerName) {
             $this->drawCard(2);
-            $playerHands[$playerName] = $this->drawnCards;
+            $playerHands[$playerName] = $this->drawnCard;
         }
         return $playerHands;
     }
--- src/tests/black_jack/DeckTest.php
+++ src/tests/black_jack/DeckTest.php
@@ -11,19 +11,20 @@ require_once(__DIR__ . '/../../lib/black_jack/Card.php');
 
 class DeckTest extends TestCase
 {
-    public function testMakeDeck()
+    public function testConstructInitializeProperty()
     {
         $deck = new Deck(new Card);
-        $shuffledDeck = $deck->makeDeck();
-        $this->assertSame(52, count($shuffledDeck));
-        $this->assertSame(count($shuffledDeck), count(array_unique($shuffledDeck)));
-        $this->assertEqualsCanonicalizing($deck->card->cards, $shuffledDeck);
+        // カードの枚数を確認
+        $this->assertSame(52, count($deck->cardDeck));
+        // カードの重複を確認
+        $this->assertSame(count($deck->cardDeck), count(array_unique($deck->cardDeck)));
+        // 処理前と処理後のカードが同じ内容か（順不同）
+        $this->assertEqualsCanonicalizing($deck->card->cards, $deck->cardDeck);
     }
 
     public function testDrawCard()
     {
         $deck = new Deck(new Card);
-        $deck->makeDeck();
         $drawnCard = $deck->drawCard(2);
 
         // カードの枚数を確認
@@ -36,7 +37,6 @@ class DeckTest extends TestCase
     public function testStartHands()
     {
         $deck = new Deck(new Card);
-        $deck->makeDeck();
         $playerHands = $deck->startHands(['takuya', 'takashi']);
 
         // 手札枚数の確認
