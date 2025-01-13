b3ea8c0f8681eff186c93979acaba478ef2ac814
takuya <cdwbh564@yahoo.co.jp>
2025-01-13 17:32:47 +0900

    feat:追加のカードを取得する処理を追加
     - `dealer`を使用して追加カードを取得
     - 取得したカードを手札に格納
     - 追加した処理のテストコード作成

--- src/tests/black_jack/PlayerTest.php
+++ src/tests/black_jack/PlayerTest.php
@@ -4,15 +4,23 @@ namespace BlackJack\Tests;
 
 use PHPUnit\Framework\TestCase;
 use BlackJack\Player;
+use BlackJack\Dealer;
+use BlackJack\Deck;
+use BlackJack\Card;
 
+require_once(__DIR__ . '/../../lib/black_jack/Card.php');
+require_once(__DIR__ . '/../../lib/black_jack/Dealer.php');
+require_once(__DIR__ . '/../../lib/black_jack/Deck.php');
 require_once(__DIR__ . '/../../lib/black_jack/Player.php');
 
 class PlayerTest extends TestCase
 {
-    public function testReceiveCard()
+    public function testAddCard()
     {
         $player = new Player('takuya');
-        $playerCard = $player->receiveCard(['takuya' => ['H2', 'H5']]);
-        $this->assertSame(['H2', 'H5'], $playerCard);
+        $dealer = new Dealer;
+        $deck = new Deck(new Card);
+        $playerCard = $player->addCard($dealer, $deck, ['H2', 'H5']);
+        $this->assertSame(3, count($playerCard));
     }
 }
