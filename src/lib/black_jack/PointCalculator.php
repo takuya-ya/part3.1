629b7eb321cc9d3f0068040a2b225af424fe1bdd
takuya <cdwbh564@yahoo.co.jp>
2025-01-13 15:35:34 +0900

    feat:手札のポイント計算処理を追加
    - 手札のrank部分を分離し、合算する処理を追加
    - 合算したスコアを返り値として設定

--- src/lib/black_jack/PointCalculator.php
+++ src/lib/black_jack/PointCalculator.php
@@ -5,6 +5,22 @@ namespace BlackJack;
 class PointCalculator {
 
     public function calculatePoint(array $hand) : int {
-        return 1;
+
+      // 手札カードをrank化。スートを除去により。
+      $ranks = [];
+      foreach($hand as $card) {
+        $rank = substr($card, 1);
+
+        // rankがJ,Q,K,Aの場合に'10'に変換
+        $aceAndFaceCards = ['J', 'Q', 'K', 'A'];
+        if (in_array($rank, $aceAndFaceCards)) {
+            $rank = 10;
+        }
+        $ranks[] = (int)$rank;
+      }
+      // 手札スコアを計算。rankを合算して。
+      $score = array_sum($ranks);
+      // 出力
+        return $score;
     }
 }
