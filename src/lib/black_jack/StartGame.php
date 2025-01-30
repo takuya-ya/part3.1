ef9e021536b226816cf73c5613031bf814c247d9
takuya <cdwbh564@yahoo.co.jp>
2025-01-30 19:56:25 +0900

    ref:ゲームの実行部分を分離
    - ゲームの実行部分と定義部分が混在しており、単一責任の原則に基づき分離
    - `StartGame.php`クラスを作成し、実行処理を移動
    
    Close #101

---
+++ src/lib/black_jack/StartGame.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlackJack;
+
+use BlackJack\Game;
+use BlackJack\Card;
+use BlackJack\Deck;
+use BlackJack\Dealer;
+use BlackJack\PointCalculator;
+use BlackJack\GameProcess;
+
+require_once(__DIR__ . '/Game.php');
+require_once(__DIR__ . '/Card.php');
+require_once(__DIR__ . '/Deck.php');
+require_once(__DIR__ . '/Dealer.php');
+require_once(__DIR__ . '/PointCalculator.php');
+require_once(__DIR__ . '/GameProcess.php');
+
+$card = new Card();
+$deckInstance = new Deck($card);
+$dealer = new Dealer();
+$pointCalculator = new PointCalculator();
+$gameProcess = new GameProcess($dealer, $deckInstance, $pointCalculator);
+$game = new Game($deckInstance, $gameProcess, $dealer, $pointCalculator, ['takuya']);
+echo $game->start();
