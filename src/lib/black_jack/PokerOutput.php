cddfa754fa2f4ab8c03e7eae9c478b14abf90edc
takuya <cdwbh564@yahoo.co.jp>
2025-02-02 17:25:10 +0900

    ref(#102):出力処理を`PokerOutput`クラスへ分離
    - 単一責任の原則に従い、`GameProcess`内での出力を分離
    - `pokerOutoput`クラスで、出力内容に応じたメソッドを実装

--- src/lib/black_jack/PokerOutput.php
+++ src/lib/black_jack/PokerOutput.php
@@ -10,4 +10,29 @@ class PokerOutput
         echo "あなたの引いたカードは{$playerHands[$playerNames[self::PLAYER_NAME_INDENT]][0]}です。" . PHP_EOL;
         echo "あなたの引いたカードは{$playerHands[$playerNames[self::PLAYER_NAME_INDENT]][1]}です。" . PHP_EOL;
     }
+
+    public function displayDealerCard(array $dealerHand): void
+    {
+        echo "ディーラーの引いたカードは{$dealerHand[0]}です。" . PHP_EOL;
+        echo 'ディーラーの引いた2枚目のカードはわかりません。' . PHP_EOL;
+        echo PHP_EOL;
+    }
+
+    public function displayDealerTurn(string $drawnLastCard): void
+    {
+        echo "ディーラーの引いたカードは{$drawnLastCard}です。" . PHP_EOL;
+    }
+
+    public function displayAddDealerCard(array $hands)
+    {
+        echo "ディーラーの引いた2枚目のカードは{$hands['dealerHand'][1]}でした。" . PHP_EOL;
+        // echo "ディーラーの現在の得点は{$dealerScore}です。" . PHP_EOL;
+        // echo PHP_EOL;
+    }
+
+    public function displayDealerScore(int $dealerScore)
+    {
+        echo "ディーラーの現在の得点は{$dealerScore}です。" . PHP_EOL;
+        echo PHP_EOL;
+    }
 }
--- src/tests/black_jack/PokerOutputTest.php
+++ src/tests/black_jack/PokerOutputTest.php
@@ -7,6 +7,14 @@ use BlackJack\PokerOutput;
 
 class PokerOutputTest extends TestCase
 {
+    private $pokerOutput;
+    public function setUp(): void
+    {
+        // TestCaseのメソッド呼出し
+        parent::setUp();
+        // デフォルトモック値を設定
+        $this->pokerOutput = new PokerOutput();
+    }
     public function testDisplayPlayerCard()
     {
         $this->expectOutputString(
@@ -16,4 +24,44 @@ class PokerOutputTest extends TestCase
         $pokerOutput = new PokerOutput();
         $pokerOutput->displayPlayerCard(['takuya' => [0, 1]], ['takuya']);
     }
+
+    public function testDisplayDealerCard()
+    {
+        $this->expectOutputString(
+            "ディーラーの引いたカードは0です。" . PHP_EOL .
+            "ディーラーの引いた2枚目のカードはわかりません。" . PHP_EOL . PHP_EOL
+        );
+        $pokerOutput = new PokerOutput();
+        $pokerOutput->displayDealerCard([0, 1]);
+    }
+
+    public function testDisplayDealerTurn()
+    {
+        $this->expectOutputString(
+            "ディーラーの引いたカードはH3です。" . PHP_EOL
+        );
+        $this->pokerOutput->displayDealerTurn('H3');
+    }
+
+    public function testDisplayAddDealerCard()
+    {
+        $this->expectOutputString(
+            "ディーラーの引いた2枚目のカードはH2でした。" . PHP_EOL
+            // "ディーラーの現在の得点は10です。" . PHP_EOL .
+            // PHP_EOL
+        );
+        $this->pokerOutput->displayAddDealerCard(['dealerHand' => ['H1', 'H2']]);
+    }
+
+    public function testDisplayDealerScore()
+    {
+        $this->expectOutputString(
+            "ディーラーの現在の得点は10です。" . PHP_EOL .
+            PHP_EOL
+        );
+        $this->pokerOutput->displayDealerScore(10);
+    }
+
+
+
 }
