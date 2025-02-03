03e5056d438c6509a08594f14cde3056bb29932c
takuya <cdwbh564@yahoo.co.jp>
2025-02-03 21:22:03 +0900

    refactor:`switch` 文を三項演算子に変更し、可読性を向上
    - コメントアウトの '10' を 10 に修正
    - マジックナンバーを定数化

--- src/lib/black_jack/PointCalculator.php
+++ src/lib/black_jack/PointCalculator.php
@@ -4,29 +4,26 @@ namespace BlackJack;
 
 class PointCalculator
 {
+    const FACE_CARD_VALUE = 10;
+    const ACE_HIGH_VALUE = 10;
+    const ACE_LOW_VALUE = 1;
+
     public function calculatePoint(array $hand): int
     {
-        // 手札カードのrankを抽出するため、スートを除去。
         $ranks = [];
         $score = 0;
+        // 手札カードのrankを抽出するため、スートを除去。
         foreach ($hand as $card) {
             $rank = substr($card, 1);
 
-            // rankがJ,Q,Kの場合に'10'に変換
+            // rankがJ,Q,Kの場合に10を代入
             $aceAndFaceCards = ['J', 'Q', 'K'];
             if (in_array($rank, $aceAndFaceCards)) {
-                $rank = 10;
+                $rank = self::FACE_CARD_VALUE;
             }
             // rankがAの場合、スコアに応じてAを変換
             if (str_contains($rank, 'A')) {
-                switch ($score) {
-                    case $score <= 11; //11以下の場合、10を追加
-                        $rank = 10;
-                        break;
-                    case $score >= 12; //12以上の場合、1を追加
-                        $rank = 1;
-                        break;
-                }
+                $rank = ($score <= 11) ? self::ACE_HIGH_VALUE : self::ACE_LOW_VALUE;
             }
             $ranks[] = (int)$rank;
             // 手札スコアを計算するため、$ranksを合算。
