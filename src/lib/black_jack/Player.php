53f468dcdef476c78ccfaaf28860b5839e330b62
takuya <cdwbh564@yahoo.co.jp>
2025-02-04 21:09:51 +0900

    ref:初回手札を取得する処理を追加

--- src/lib/black_jack/Player.php
+++ src/lib/black_jack/Player.php
@@ -8,9 +8,17 @@ use BlackJack\Deck;
 class Player
 {
     public array $hand = [];
-
-    public function __construct(public string $playerName)
+    public function __construct(
+        public Dealer $dealer,
+        public Deck $deck,
+        public string $playerName
+        )
     {
+    }
+
+    public function getHand(): array
+    {
+        return $this->dealer->dealStartHands($this->deck);
     }
 
     public function addCard(Dealer $dealer, Deck $deck, array $playerHand): array
