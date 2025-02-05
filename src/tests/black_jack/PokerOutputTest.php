22af1150d8845d7cd378e38e79ae2ee7fb626be8
takuya <cdwbh564@yahoo.co.jp>
2025-02-05 14:48:25 +0900

    feat:複数プレイヤー対応のため、出力内容にプレイヤー名を追加
    - 出力メッセージで各プレイヤー名を表示

--- src/tests/black_jack/PokerOutputTest.php
+++ src/tests/black_jack/PokerOutputTest.php
@@ -18,8 +18,8 @@ class PokerOutputTest extends TestCase
     public function testDisplayPlayerCard()
     {
         $this->expectOutputString(
-            "あなたの引いたカードは0です。" . PHP_EOL .
-            "あなたの引いたカードは1です。" . PHP_EOL
+            "takuyaの引いた1枚目のカードは0です。" . PHP_EOL .
+            "takuyaの引いた2枚目のカードは1です。" . PHP_EOL
         );
         $pokerOutput = new PokerOutput();
         $pokerOutput->displayPlayerCard(['takuya' => [0, 1]], ['takuya']);
