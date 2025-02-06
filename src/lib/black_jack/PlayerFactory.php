8b531a3a90c65cdd4822561f00b7baced137f136
takuya <cdwbh564@yahoo.co.jp>
2025-02-06 18:57:22 +0900

    ref:初回カード取得処理を`PlayerFactory`に切り出し
    - 責任の分離と、コードの可読性を向上

---
+++ src/lib/black_jack/PlayerFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BlackJack;
+
+use BlackJack\Dealer;
+use BlackJack\Deck;
+
+class PlayerFactory
+{
+      public $players = [];
+      // 各プレイヤーのインスタンス作成
+      public function __construct(private Dealer $dealer, private Deck $deck, private array $playerNames)
+      {
+        foreach ($this->playerNames as $playerName) {
+            $this->players[$playerName] = new Player($this->dealer, $this->deck, $playerName);
+        }
+      }
+}
