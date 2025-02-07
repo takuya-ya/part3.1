d44034b5b9b20c0b644c3e1631bb8f4b0783418b
takuya <cdwbh564@yahoo.co.jp>
2025-02-07 17:04:25 +0900

    ref:出力処理の分離、操作プレイヤー名の呼出し方法を統一

--- src/lib/black_jack/Game.php
+++ src/lib/black_jack/Game.php
@@ -20,21 +20,22 @@ class Game
     ) {
     }
 
-    private string $yourName;
+    const CONTROL_PLAYER_NUMBER = 0;
     public function start(): string
     {
+        $yourName = $this->playerNames[self::CONTROL_PLAYER_NUMBER];
         $playerInstances = $this->playerFactory->players;
 
         echo 'ブラックジャックを開始します。' . PHP_EOL;
         echo PHP_EOL;
 
         // 初回カード取得
+        // プレイヤー達とディーラーの手札の配列
         $hands = $this->gameProcess->setUpHands($playerInstances);
         // プレイヤーの追加カード取得。バーストしている場合は文字列の為、変数名をScoreでなくResultに設定
-        $playerResult = $this->gameProcess->addYourCard($hands, $this->yourName, $playerInstances[0]);
-        if ($playerResult === 'あなたの負けです。') {
-            echo "$playerResult" . PHP_EOL;
-            return 'ブラックジャックを終了します。' . PHP_EOL;
+        $playerResult = $this->gameProcess->addYourTurn($hands['playerHands'][$yourName],   $playerInstances[$yourName]);
+        if ($this->isGameOrver($playerResult)) {
+            return $this->yourLose($playerResult);
         }
 
         // ディーラーのカード追加処理
@@ -48,4 +49,21 @@ class Game
         $this->gameProcess->judgeWinner($playerResult, $dealerScore, $this->playerNames);
         return 'ブラックジャックを終了します。' . PHP_EOL;
     }
+
+    private function isGameOrver(string | int $playerResult): bool
+    {
+        if($playerResult == 'あなたの負けです。') {
+            return true;
+        }
+        return false;
+    }
+
+    private function yourLose(string | int $playerResult): string
+    {
+        if ($playerResult === 'あなたの負けです。') {
+            echo "$playerResult" . PHP_EOL;
+            return 'ブラックジャックを終了します。' . PHP_EOL;
+        }
+    }
+
 }
