1384d4155346990ec35358d2bceac69f198b5b71
takuya <cdwbh564@yahoo.co.jp>
2025-02-07 17:03:08 +0900

    ref:addCardで追加カードを返し、`addYourTurn`で手札に追加する処理に変更
    - `addCard`の引数削減と責任の明確化

--- src/tests/black_jack/GameProcessTest.php
+++ src/tests/black_jack/GameProcessTest.php
@@ -18,6 +18,7 @@ class GameProcessTest extends TestCase
     private $dealer;
     private $pointCalculator;
     private $pokerOutput;
+    private $dealerMock;
 
     public function setUp(): void
     {
@@ -26,9 +27,12 @@ class GameProcessTest extends TestCase
         // デフォルトモック値を設定
         $this->handle = fopen('php://temp', 'r+');
         $this->deck = new Deck(new Card());
-        $this->dealer = new Dealer();
+        $this->dealer = new Dealer($this->deck);
         $this->pointCalculator = new PointCalculator();
         $this->pokerOutput = new PokerOutput();
+
+        $this->dealerMock = $this->createMock(Dealer::class);
+        $this->dealerMock->method('dealAddCard')->willReturn(['H6']);
     }
 
     public function tearDown(): void
@@ -43,15 +47,14 @@ class GameProcessTest extends TestCase
         $expectedPlayer = ['H1', 'D3'];
         $expectedDealer = ['K1', 'K2'];
         $playerMock = $this->getMockBuilder(Player::class)
-                ->setConstructorArgs([$this->dealer, $this->deck, 'takuya'])
+                ->setConstructorArgs([$this->dealer, 'takuya'])
                 ->getMock(); //モックインスタンスの作成
         $playerMock->method('getHand')->willReturn($expectedPlayer);
 
-        $dealerMock = $this->createMock(Dealer::class);
-        $dealerMock->method('dealStartHands')->willReturn($expectedDealer);
+        $this->dealerMock->method('dealStartHands')->willReturn($expectedDealer);
 
         // 返り値として、プライヤーとディーラーの手札を含む配列を確認
-        $gameProcess = new GameProcess($dealerMock, $this->deck, $this->pointCalculator, $this->pokerOutput);
+        $gameProcess = new GameProcess($this->dealerMock, $this->deck, $this->pointCalculator, $this->pokerOutput);
         $hands = $gameProcess->setUpHands([$playerMock]);
         $this->assertSame(['playerHands' => ['takuya' => $expectedPlayer], 'dealerHand' => $expectedDealer], $hands);
 
@@ -60,57 +63,46 @@ class GameProcessTest extends TestCase
 
     public function testDealerTurn()
     {
-        $mock = $this->createMock(Dealer::class);
-        $mock->method('dealAddCard')->willReturn(['H6']);
-
-        $gameProcess = new GameProcess($mock, $this->deck, $this->pointCalculator, $this->pokerOutput);
+        $gameProcess = new GameProcess($this->dealerMock, $this->deck, $this->pointCalculator, $this->pokerOutput);
         $dealerScore = $gameProcess->dealerTurn(['D1', 'DA'], 11);
         $this->assertSame(23, $dealerScore);
     }
 
     public function testAddDealerCard()
     {
-        $dealerMock = $this->getMockBuilder(Dealer::class)
-            ->onlyMethods(['dealAddCard'])
-            ->getMock();
-        $dealerMock->method('dealAddCard')->willReturn(['H10']);
-
-        $gameProcess = new GameProcess($dealerMock, $this->deck, $this->pointCalculator, $this->pokerOutput);
+        $gameProcess = new GameProcess($this->dealerMock, $this->deck, $this->pointCalculator, $this->pokerOutput);
         // 返り値を確認
-        $dealerScore = $gameProcess->addDealerCard(['dealerHand' => ['D6', 'D5']]);
+        $dealerScore = $gameProcess->addDealerCard(['dealerHand' => ['D10', 'D5']]);
         $this->assertSame(21, $dealerScore);
         // バーストした場合
-        $dealerScore = $gameProcess->addDealerCard(['dealerHand' => ['D6', 'D7']]);
+        $dealerScore = $gameProcess->addDealerCard(['dealerHand' => ['D10', 'D7']]);
         $this->assertSame('あなたの勝ちです。', $dealerScore);
 
     }
 
-    public function testAddYourCard()
+    public function testAddYourTurn()
     {
-        $dealerMock = $this->createMock(Dealer::class);
-        $dealerMock->method('dealAddCard')->willReturnOnConsecutiveCalls(['D3'], ['K10']);
+        // setUpでのaddCardメソッドの帰り値が上書き出来なかった為、モックを初期化
+        $this->dealerMock = $this->createMock(Dealer::class);
+        $this->dealerMock->method('dealAddCard')
+                ->willReturnOnConsecutiveCalls(['D3'], ['K10']);
+
+        $player = new Player($this->dealerMock, 'takuya');
 
         // 返り値の確認
         fwrite($this->handle, "Y\nN\n"); // ユーザー入力の代替値を設定
         rewind($this->handle); //ストリームポインタをリセット
 
-        $gameProcess = new GameProcess($dealerMock, $this->deck, $this->pointCalculator, $this->pokerOutput, $this->handle);
-        $playerScore = $gameProcess->addYourCard(
-            ['playerHands' => ['takuya' => ['D6', 'D7']]],
-            'takuya',
-            new Player($dealerMock, $this->deck, 'takuya')
-        );
+        $gameProcess = new GameProcess($this->dealerMock, $this->deck, $this->pointCalculator, $this->pokerOutput, $this->handle);
+
+        $playerScore = $gameProcess->addYourTurn(['D6', 'D7'], $player);
         $this->assertSame(16, $playerScore);
 
         // スコアが21を超えた場合に、バースト判定の確認
         fwrite($this->handle, "Y\nY\n");
         rewind($this->handle);
 
-        $message = $gameProcess->addYourCard(
-            ['playerHands' => ['takuya' => ['D6', 'D7']]],
-            'takuya',
-            new Player($dealerMock, $this->deck, 'takuya')
-        );
+        $message = $gameProcess->addYourTurn(['D6', 'D7'], $player);
         $this->assertSame('あなたの負けです。', $message);
     }
 
