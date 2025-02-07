9210cd6fc5e66aa88d0cd4409815a89ac2406b24
takuya <cdwbh564@yahoo.co.jp>
2025-02-07 11:49:45 +0900

    ref:プレイヤーの追加カード取得時の出力を変更
    - 複数プレイヤー対応のによるdisplayYourLoseMessage変更の為

--- src/lib/black_jack/PlayerFactory.php
+++ src/lib/black_jack/PlayerFactory.php
@@ -7,12 +7,12 @@ use BlackJack\Deck;
 
 class PlayerFactory
 {
-      public $players = [];
-      // 各プレイヤーのインスタンス作成
-      public function __construct(private Dealer $dealer, private Deck $deck, private array $playerNames)
-      {
-        foreach ($this->playerNames as $playerName) {
-            $this->players[$playerName] = new Player($this->dealer, $this->deck, $playerName);
-        }
+    public $players = [];
+    // 各プレイヤーのインスタンス作成
+    public function __construct(private Dealer $dealer, private Deck $deck, private array $playerNames)
+    {
+      foreach ($this->playerNames as $playerName) {
+          $this->players[$playerName] = new Player($this->dealer, $this->deck, $playerName);
       }
+    }
 }
