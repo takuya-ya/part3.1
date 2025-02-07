416550de0efefda82956ae14f175c0a884903429
takuya <cdwbh564@yahoo.co.jp>
2025-02-07 14:54:06 +0900

    ref:メソッドの引数を削減し、冗長性の低下
    - インスタンス生成時に引数を指定し、メソッド引数での指定削除。これにより、複数回メソッド呼出す場合の冗長性を排除。

--- src/tests/black_jack/PlayerTest.php
+++ src/tests/black_jack/PlayerTest.php
@@ -21,22 +21,16 @@ class PlayerTest extends TestCase
         $dealerMock->method('dealStartHands')
                    ->willReturn($expectedCards); // Dealer がカードを渡すと仮定
 
-        $deckMock = $this->createMock(Deck::class);
-
         // Player インスタンスを実際に作成
-        $player = new Player($dealerMock, $deckMock, 'takuya');
+        $player = new Player($dealerMock, 'takuya');
         $player->drawStartHand();
         $this->assertSame($expectedCards, $player->getHand());
-
     }
 
-        // TODO：要修正
-    // public function testAddCard()
-    // {
-    //     $player = new Player('takuya');
-    //     $dealer = new Dealer();
-    //     $deck = new Deck(new Card());
-    //     $playerCard = $player->addCard($dealer, $deck, ['H2', 'H5']);
-    //     $this->assertSame(3, count($playerCard));
-    // }
+    public function testAddCard()
+    {
+        $player = new Player(new Dealer(new Deck(new Card)), 'takuya');
+        $playerCard = $player->addCard();
+        $this->assertSame(3, count($playerCard));
+    }
 }
