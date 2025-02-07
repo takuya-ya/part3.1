8358a5fe4f5b89af48fc6710720cf33eab17b4ce
takuya <cdwbh564@yahoo.co.jp>
2025-02-07 14:54:06 +0900

    ref:メソッドの引数を削減し、冗長性の低下
    - インスタンス生成時に引数を指定し、メソッド引数での指定削除。これにより、複数回メソッド呼出す場合の冗長性を排除。

--- src/lib/black_jack/Player.php
+++ src/lib/black_jack/Player.php
@@ -3,14 +3,12 @@
 namespace BlackJack;
 
 use BlackJack\Dealer;
-use BlackJack\Deck;
 
 class Player
 {
     private array $hand = [];
     public function __construct(
         public Dealer $dealer,
-        public Deck $deck,
         public string $playerName
         )
     {
@@ -25,12 +23,11 @@ class Player
     // 初回手札を取得
     public function drawStartHand(): void
     {
-        $this->hand = $this->dealer->dealStartHands($this->deck);
+        $this->hand = $this->dealer->dealStartHands();
     }
 
-    public function addCard(Dealer $dealer, Deck $deck, array $playerHand): array
+    public function addCard(): array
     {
-        $playerHand = array_merge($playerHand, $dealer->dealAddCard($deck));
-        return $playerHand;
+        return $this->dealer->dealAddCard();
     }
 }
